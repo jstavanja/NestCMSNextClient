541f7f06a72a1e1adaff56571c4ad7b474c94d0c
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2021-09-05 00:21:25 +0200

    :sparkles: Make the admin panel greet a user on index

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,28 +1,15 @@
-import { useContext, useEffect } from 'react'
-
+import { observer } from 'mobx-react';
+import { useContext } from 'react';
 import AdminLayout from '../../layouts/admin'
-import UserStore from '../../stores/userStore'
-import CommonStore from '../../stores/commonStore'
+import UserStore from '../../stores/userStore';
 
-const Home = () => {
+const Home = observer(() => {
   const userStore = useContext(UserStore)
-  const commonStore = useContext(CommonStore)
-
-  const pullUser = async () => {
-    if (commonStore.token) {
-      await userStore.pullUser()
-      await commonStore.setAppLoaded()
-    }
-  }
-
-  useEffect(() => {
-    pullUser()
-  }, [])
 
   return (
     <>
       <AdminLayout title='Dashboard'>
-        <div className='page-content'>Welcome to the admin panel.</div>
+        <div className='page-content'>Welcome to the admin panel, {userStore?.currentUser?.username}!</div>
       </AdminLayout>
       <style jsx>{`
         .page-content {
@@ -31,6 +18,6 @@ const Home = () => {
       `}</style>
     </>
   )
-}
+})
 
 export default Home
