c253fdac42b0860f413815c5bb4fcc3132ec9afb
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2021-09-24 21:40:16 +0200

    :sparkles: Style the create page form with Chakra

--- pages/admin/pages/create.jsx
+++ pages/admin/pages/create.jsx
@@ -1,8 +1,18 @@
 import AdminLayout from '../../../layouts/admin'
 import { store as createPage } from '../../../services/pages'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Container, VStack } from '@chakra-ui/layout';
+import Link from '../../../components/Link';
+import { Button } from '@chakra-ui/button';
+import { FormControl, FormErrorMessage, FormLabel } from '@chakra-ui/form-control';
+import { Input } from '@chakra-ui/input';
+import { Textarea } from '@chakra-ui/textarea';
+import { Checkbox } from '@chakra-ui/checkbox';
+import { useRouter } from 'next/router';
 
 const PageCreate = () => {
+
+	const router = useRouter()
 
 	const validatePageCreateForm = values => {
 		const errors = {};
@@ -25,40 +35,66 @@ const PageCreate = () => {
 
 	return (
 		<>
-		<AdminLayout title={`Create page`}>
-			<div className="page-content">
-				<Formik
-					initialValues={{ title: '', content: '', isPublished: true }}
-					validate={validatePageCreateForm}
-					onSubmit={submitPageCreateForm}
-				>
-					{({ isSubmitting }) => (
-						<Form>
-							<label htmlFor="title">Title</label>
-							<Field type="text" name="title"></Field>
-							<ErrorMessage name="title" component="div" />
-							<label htmlFor="permalink">Permalink</label>
-							<Field type="text" name="permalink"></Field>
-							<ErrorMessage name="permalink" component="div" />
-							<label htmlFor="content">Content</label>
-							<Field as="textarea" name="content"></Field>
-							<ErrorMessage name="content" component="div" />
-							<label htmlFor="isPublished">Is published</label>
-							<Field type="checkbox" name="isPublished" />
-							<button type="submit" disabled={isSubmitting}>
-								Create
-							</button>
-							<ErrorMessage name="isPublished" component="div" />
-						</Form>
-					)}
-				</Formik>
-			</div>
-		</AdminLayout>
-		<style jsx>{`
-			.page-content {
-				padding: 50px;
-			}
-		`}</style>
+			<AdminLayout title={`Create page`}>
+				<Container mt="40px">
+					<Link href='../pages'>
+						<Button size="sm">&lt;- Back to the list of pages</Button>
+					</Link>
+					<Container mt="20px">
+						<Formik
+							initialValues={{ title: '', content: '', permalink: '', isPublished: true }}
+							validate={validatePageCreateForm}
+							onSubmit={submitPageCreateForm}
+						>
+							{({ isSubmitting }) => (
+								<Form>
+									<VStack>
+										<Field type="text" name="title">
+											{({ field, form }) => (
+												<FormControl isInvalid={form.errors.title && form.touched.title}>
+													<FormLabel htmlFor="title">Title</FormLabel>
+													<Input {...field} id="title" placeholder="Title" />
+													<FormErrorMessage>{form.errors.title}</FormErrorMessage>
+												</FormControl>
+											)}
+										</Field>
+										<Field type="text" name="permalink">
+											{({ field, form }) => (
+												<FormControl isInvalid={form.errors.permalink && form.touched.permalink}>
+													<FormLabel htmlFor="permalink">Permalink (slug)</FormLabel>
+													<Input {...field} id="permalink" placeholder="page_slug" />
+													<FormErrorMessage>{form.errors.permalink}</FormErrorMessage>
+												</FormControl>
+											)}
+										</Field>
+										<Field type="textarea" name="content">
+											{({ field, form }) => (
+												<FormControl isInvalid={form.errors.content && form.touched.content}>
+													<FormLabel htmlFor="content">Content</FormLabel>
+													<Textarea {...field} id="content" name="content" />
+													<FormErrorMessage>{form.errors.content}</FormErrorMessage>
+												</FormControl>
+											)}
+										</Field>
+										<Field type="checkbox" name="isPublished">
+											{({ field, form }) => (
+												<FormControl isInvalid={form.errors.content && form.touched.content}>
+													<FormLabel htmlFor="content">Is published</FormLabel>
+													<Checkbox {...field} id="is_published" name="is_published" />
+													<FormErrorMessage>{form.errors.isPublished}</FormErrorMessage>
+												</FormControl>
+											)}
+										</Field>
+										<Button type="submit" isLoading={isSubmitting}>
+											Create
+										</Button>
+									</VStack>
+								</Form>
+							)}
+						</Formik>
+					</Container>
+				</Container>
+			</AdminLayout>
 		</>
 	)
 }
