7340acdbb5593e0b8081d99a2ce1cd3159b130c8
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2021-11-01 19:43:53 +0100

    :sparkles: use SWR for pages list data fetching

--- pages/admin/pages/index.jsx
+++ pages/admin/pages/index.jsx
@@ -1,42 +1,16 @@
-import { useContext, useEffect, useState } from 'react';
 import AdminLayout from '../../../layouts/admin'
 import Link from '../../../components/Link'
 import { Container, VStack } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/button';
 import PageInfo from '../../../components/PageInfo'
 import { observer } from 'mobx-react';
-import PagesStore from '../../../stores/pagesStore';
-import { useToast } from '@chakra-ui/toast';
+import useSWR from 'swr';
+import { PATHS } from "../../../constants/api";
+import { fetcher } from '../../../utils/fetcher';
 
 const PageIndex = observer(() => {
 
-  const [pagesFetchError, setPagesFetchError] = useState(null)
-
-  const pagesStore = useContext(PagesStore)
-  const toast = useToast()
-
-  const createPrettyPagesFetchErrorMessage = (errorMessage) =>
-    `The pages list cannot be fetched. Try again later. Reason: ${errorMessage}`
-
-  const fetchPages = async () => {
-    try {
-      await pagesStore.fetchPages()
-    } catch (error) {
-      setPagesFetchError(error.message)
-      toast({
-        title: "Could not fetch pages.",
-        description: createPrettyPagesFetchErrorMessage(error.message),
-        status: "error",
-        duration: 3000,
-        isClosable: true,
-      })
-    }
-  }
-
-
-  useEffect(() => {
-    fetchPages()
-  }, [])
+  const { data: pages, error } = useSWR(PATHS.PAGES.INDEX, fetcher)
 
   return (
     <>
@@ -47,9 +21,13 @@ const PageIndex = observer(() => {
           </Link>
           <VStack align="stretch" as="ul" mt="20px">
             {
-              !pagesFetchError
-                ? pagesStore.pages.map(page => (<PageInfo key={page.id} as='li' page={page}/>))
-                : <p>{createPrettyPagesFetchErrorMessage(pagesFetchError)}</p>  
+              error && <p>Couldn't fetch pages.</p>
+            }
+            {
+              (!pages && !error) && <p>Pages loading...</p>
+            }
+            {
+              pages && pages.map(page => (<PageInfo key={page.id} as='li' page={page}/>))
             }
           </VStack>
         </Container>
